cd971d377e7a6257791cce0e9a45cb448b70a2ec
Stuart Blackburn <stuart@dtangl.com>
2024-06-10 03:28:49 +0100

    task: added TestGameConsole

---
+++ src/MindSweeper.Cli/GameTestConsole.cs
@@ -0,0 +1,11 @@
+﻿namespace MindSweeper.Cli;
+
+/// <summary>
+/// Represents a game console for the MindSweeper game.
+/// </summary>
+/// <remarks>
+/// This class is a wrapper around the <see cref="TestConsole"/> class and implements the <see cref="IGameConsole"/> interface.
+/// </remarks>
+internal class GameTestConsole : TestConsole, IGameConsole
+{
+}
--- src/MindSweeper.Cli/IGameConsole.cs
+++ src/MindSweeper.Cli/IGameConsole.cs
@@ -3,36 +3,59 @@
 /// <summary>
 /// Represents a game console interface.
 /// </summary>
+/// <remarks>
+/// Provides methods to facilitate the sharing of the implementations between <see cref="GameConsole"/> and <see cref="GameTestConsole"/>
+/// </remarks>
 internal interface IGameConsole : IConsole
 {
     /// <summary>
     /// Writes a string value to the console.
     /// </summary>
     /// <param name="value">The string value to write.</param>
-    void Write(string value);
+    void Write(string value) => Out.Write(value);
 
     /// <summary>
-    /// Writes a formatted string to the console using the specified format string and arguments.
+    /// Writes a formatted string to the console.
     /// </summary>
-    /// <param name="format">The format string.</param>
-    /// <param name="args">The arguments to format.</param>
-    void Write(string format, params object?[] args);
+    /// <param name="format">A composite format string.</param>
+    /// <param name="args">An array of objects to format.</param>
+    void Write(string format, params object?[] args)
+    {
+        if (args.Length == 0)
+        {
+            Out.Write(format);
+            return;
+        }
+
+        var formatted = string.Format(format, args);
+        Out.Write(formatted);
+    }
 
     /// <summary>
     /// Writes a new line to the console.
     /// </summary>
-    void WriteLine();
+    void WriteLine() => Out.WriteLine();
 
     /// <summary>
     /// Writes a string value followed by a new line to the console.
     /// </summary>
     /// <param name="value">The string value to write.</param>
-    void WriteLine(string value);
+    void WriteLine(string value) => Out.WriteLine(value);
 
     /// <summary>
-    /// Writes a formatted string followed by a new line to the console using the specified format string and arguments.
+    /// Writes a formatted string followed by a new line to the console.
     /// </summary>
-    /// <param name="format">The format string.</param>
-    /// <param name="args">The arguments to format.</param>
-    void WriteLine(string format, params object?[] args);
+    /// <param name="format">A composite format string.</param>
+    /// <param name="args">An array of objects to format.</param>
+    void WriteLine(string format, params object?[] args)
+    {
+        if (args.Length == 0)
+        {
+            Out.WriteLine(format);
+            return;
+        }
+
+        var formatted = string.Format(format, args);
+        Out.WriteLine(formatted);
+    }
 }
